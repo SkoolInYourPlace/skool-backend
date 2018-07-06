83449d21bf14a6576c01179e43676587289efd28
Vishnu M V <a-8244@LE1040.local>
2018-07-06 15:40:43 +0530

    Allow cross origin - temp

--- src/main/java/com/startup/daddyschool/controller/SchoolController.java
+++ src/main/java/com/startup/daddyschool/controller/SchoolController.java
@@ -6,13 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.startup.daddyschool.entity.SchoolEntity;
@@ -55,6 +49,7 @@ public class SchoolController {
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}*/	
 	@GetMapping("school")
+	@CrossOrigin
 	public ResponseEntity<List<SchoolEntity>> getAllSchools() {
 		List<SchoolEntity> list = schoolService.getAllSchools();
 		return new ResponseEntity<List<SchoolEntity>>(list, HttpStatus.OK);
